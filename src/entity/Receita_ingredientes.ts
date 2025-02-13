8a6b09500eddf8171eb72cc81f98697035717f4a
Lucas Benkendorf <benklucass@gmail.com>
2025-02-13 09:57:07 -0300

    criado entidade e migration para receitas_ingredientes

---
+++ src/entity/Receita_ingredientes.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("recipes_ingredients")
+export class RecipeIngredient {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: "varchar", length: 100, nullable: false })
+  name: string;
+
+  @CreateDateColumn({ type: "timestamp" })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: "timestamp" })
+  updated_at: Date;
+}
