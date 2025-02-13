0842db4aa3268a808cb30bd0b49a1b73aa948d67
Lucas Benkendorf <benklucass@gmail.com>
2025-02-13 10:54:52 -0300

    criado o controller do projeto

--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -1,6 +1,31 @@
+import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
+import { Receita } from "../entity/Receita";
+import { EntityManager } from "typeorm";
 
-class RecipeController {
- 
+export class RecipeController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { name, preparation_time, is_fitness } = request.body;
+    const queryRunner = AppDataSource.createQueryRunner();
+
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      const recipe = new Receita();
+      recipe.name = name;
+      recipe.preparation_time = preparation_time;
+      recipe.is_fitness = is_fitness;
+
+      await queryRunner.manager.save(recipe);
+      await queryRunner.commitTransaction();
+
+      return response.status(201).json(recipe);
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      return response.status(500).json({ message: "Erro ao criar receita", error });
+    } finally {
+      await queryRunner.release();
+    }
+  }
 }
-
-export default RecipeController;
